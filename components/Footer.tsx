73be77bac0939f2b1dc1ce8dbd58be97dad5622f
titantooth <ali.rabieeie@gmail.com>
2023-08-30 16:28:06 +0330

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image 
+          src='/logo-purple.svg'
+          width={115}
+          height={38}
+          alt='incredibbble'
+          />
+          <p className='text-start text-sm mt-5 max-w-xs font-normal'>
+            Incredibbble is the worlds leading commulutyu in 
+          </p>
+            
+          
+        </div>
+      </div>
+    </footer>
+
   )
 }
 
