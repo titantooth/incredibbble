a85d28b92e7035f3dfefb73f9e1d3dd9bdca1d81
titantooth <ali.rabieeie@gmail.com>
2023-08-30 13:28:21 +0330

    made structure - navbar main footer

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
+
+export const metadata = {
+  title: 'Incredibbble',
+  description: 'Showcase and discover great projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main>
+
+          {children}
+
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  )
+}
