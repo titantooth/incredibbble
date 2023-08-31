7eed325734d92b838853f1b64e822573f3e556ea
titantooth <ali.rabieeie@gmail.com>
2023-08-31 10:04:45 +0330

    added google auth cliet id secret to env added to route added to session

--- lib/session.ts
+++ lib/session.ts
@@ -8,28 +8,34 @@ import {JWT} from 'next-auth/jwt'
 export const authOptions : NextAuthOptions = {
     providers : [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId:  process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
         })
     ],
-    jwt : {
-        encode : ({secret, token}) => {
+    // jwt : {
+    //     encode : ({secret, token}) => {
 
-        },
-        decode : async ({secret, token}) => {
+    //     },
+    //     decode : async ({secret, token}) => {
 
-        }
-    },
+    //     }
+    // },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
     },
     callbacks : {
         async session({session}) {
-
+            return session
         },
-        async signIn({user}) {
-            
+        async signIn({user} : {user: AdapterUser | User}) {
+            try {
+                 
+                return true
+            } catch (error : any) {
+                console.log(error)
+                return false
+            }
         }
     }
 }
